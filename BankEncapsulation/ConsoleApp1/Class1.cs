939f8c13e252a7664a13a29aae9de54ebb835d80
Brian Henderson <henderson.brian82@gmail.com>
2020-09-02 12:41:50 -0400

    Banking Encapsulation Practice

---
+++ BankEncapsulation/ConsoleApp1/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+        }
+        
+        private double balance = 0;
+        public double Balance
+        {
+            get
+            {
+                return balance;
+            }
+
+            set
+            {
+                balance = value;
+            }
+        }
+
+        public void Deposit(double depositAmount)
+        {
+           balance = depositAmount;
+        }
+
+        public string GetBalance()
+        {
+            return $"${balance}";
+        }
+    }
+}
---
+++ BankEncapsulation/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount();
+            Console.WriteLine("How much will you deposit today?");
+            var amountToDeposit = double.Parse(Console.ReadLine());
+            account.Deposit(amountToDeposit);
+            Console.WriteLine($"Thank you.  Your balance is now {account.GetBalance()}");
+        }
+    }
+}

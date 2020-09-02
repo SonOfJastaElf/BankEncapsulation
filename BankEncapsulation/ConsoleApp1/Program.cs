2dd6c111fa748eb98b24c6ea4ef49b2237ca03dc
Brian Henderson <henderson.brian82@gmail.com>
2020-09-02 15:51:56 -0400

    Included personal info class

--- BankEncapsulation/ConsoleApp1/Program.cs
+++ BankEncapsulation/ConsoleApp1/Program.cs
@@ -6,6 +6,20 @@ namespace BankEncapsulation
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Thank you for choosing National Hendersonian Bank.  To creat an account, we'll need to know some things about you, including your phne number:");
+            var newMember = new PersonalInfo
+            {
+                FirstName = "Allen",
+                LastName = "Boyd",
+                StreetAddress = "5539 Starboard Ct",
+                Address2 = "N/A",
+                City = "Fairfax",
+                State  ="VA",
+                Zip = 22032
+            };
+            newMember.NumberFormatted = Console.ReadLine();
+            Console.WriteLine($"Got it!  Again, that number is {newMember.NumberFormatted}");
+            Console.WriteLine("----------------------------------------");
             var account = new BankAccount();
             Console.WriteLine("Welcome.  How much will you deposit today?");
             var amountToDeposit = double.Parse(Console.ReadLine());
